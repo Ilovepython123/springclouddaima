ed184b4c7cb1147e85fe1398c5677653451514fd
lihuan <1634712898@qq.com>
2021-05-28 10:39:38 +0800

    互相调用

--- order_service/src/main/java/cn/itcast/order/controller/OrderController.java
+++ order_service/src/main/java/cn/itcast/order/controller/OrderController.java
@@ -2,6 +2,8 @@ package cn.itcast.order.controller;
 
 import cn.itcast.order.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +20,15 @@ public class OrderController {
 	//注入restTemplate对象
 	@Autowired
 	private RestTemplate restTemplate;
+
+	/**
+	 * 注入DiscoveryClient :
+	 *  springcloud提供的获取原数组的工具类
+	 *      调用方法获取服务的元数据信息
+	 *
+	 */
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	/**
 	 * 注入DiscoveryClient :
@@ -55,25 +66,29 @@ public class OrderController {
 	 */
 	@RequestMapping(value = "/buy/{id}",method = RequestMethod.GET)
 	public Product findById(@PathVariable Long id) {
-//		//调用discoveryClient方法
-//		//已调用服务名称获取所有的元数据
-//		List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
-//		//获取唯一的一个元数据
-//		ServiceInstance instance = instances.get(0);
-//		//根据元数据中的主机地址和端口号拼接请求微服务的URL
-//		Product product = null;
+		//调用discoveryClient方法
+		//已调用服务名称获取所有的元数据
+		List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+		//获取唯一的一个元数据
+		ServiceInstance instance = instances.get(0);
+		//根据元数据中的主机地址和端口号拼接请求微服务的URL
+		String host = instance.getHost();
+		int port = instance.getPort();
+		Product product = null;
+
 //		String host = instance.getHost();
 //		int port = instance.getPort();
-////		product = restTemplate.getForObject("http://127.0.0.1:9011/product/2",Product.class);
+//		product = restTemplate.getForObject("http://127.0.0.1:9011/product/2",Product.class);
 //
 //		//如何调用商品服务?
 //		URI uri = URI.create("http://"+instance.getHost()+":"+instance.getPort()+"/product/2");
 //		product = restTemplate.getForObject(uri, Product.class);
-////		product = restTemplate.getForObject("http://"+instance.getHost()+":"+instance.getPort()+"/product/2",Product.class);
-//		return product;
-		Product product = null;
-		product = restTemplate.getForObject("http://127.0.0.1:9011/product/2", Product.class);
+		product = restTemplate.getForObject("http://"+instance.getHost()+":"+instance.getPort()+"/product/2"
+				,Product.class);
 		return product;
+//		Product product = null;
+//		product = restTemplate.getForObject("http://127.0.0.1:9011/product/2", Product.class);
+//		return product;
 	}
 
 
